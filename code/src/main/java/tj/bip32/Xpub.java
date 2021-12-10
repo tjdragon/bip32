76eaa1a317978c5c10c0e5e574c16023a0dc3ed4
tjdragon <thierry@janaudy.com>
2021-12-10 15:34:54 +0100

    Misc re XPUB

---
+++ code/src/main/java/tj/bip32/Xpub.java
@@ -0,0 +1,10 @@
+package tj.bip32;
+
+public class Xpub {
+    public static void main(String[] args) throws Exception {
+        final ExtKey xpubk1 = ExtKey.fromBase58("xpub6AKC3u8URPxDojLnFtNdEPFkNsXxHfgRhySvVfEJy9SVvQAn14XQjAoFY48mpjgutJNfA54GbYYRpR26tFEJHTHhfiiZZ2wdBBzydVp12yU");
+
+        final ExtKey xpubk2 = ExtKey.fromBase58("SZx1z48947KgeoZ7tHT2SSs97M4XqduB5NYvZadg7WDRFeyhytNZ1tbRq3WsB5PxhQ7eK6QaT2ZTMEkpsGiTegSsxrpky7LUE44gUyuz5sFXqtcu");
+
+    }
+}
